f768291ca78441b9593773547693b4a7f50eff66
phillima <phyllipe@unifei.edu.br>
2025-06-13 17:46:57 -0300

    Update Pokemon

--- aula-15-nextjs-update-delete/src/app/dashboard/edit/[id]/page.tsx
+++ aula-15-nextjs-update-delete/src/app/dashboard/edit/[id]/page.tsx
@@ -1,3 +1,12 @@
+
+import ConexaoBD from "@/app/libs/conexao-bd";
+import "@/app/styles/create-pokemon.css";
+import { PokemonProps } from "@/app/ui/pokemon-card";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+const arquivo = 'pokemon-db.json';
+
 interface EditPokemonProps{
     params: Promise<{id: string}>;
 }
@@ -5,9 +14,70 @@ interface EditPokemonProps{
 export default async function EditPokemon({params}: EditPokemonProps){
 
     const {id} = await params;
-    console.log(id);
+    
+    const pokemonDB = await ConexaoBD.retornaBD(arquivo);
+
+    const pokemonToEdit: PokemonProps = pokemonDB.find((p: PokemonProps) => p.id === id);
+    const pokemonToEditIndex: number = pokemonDB.findIndex((p) => p.id === id);
+
+    const updatePokemon = async (formData : FormData) => {
+        'use server';
+
+        //
+        const updatedPokemon: PokemonProps = {
+            id,
+            nome: formData.get('nome') as string,
+            descricao: formData.get('descricao') as string,
+            img: formData.get('img') as string
+        }
+
+        pokemonDB.splice(pokemonToEditIndex,1,updatedPokemon);
+
+        await ConexaoBD.armazenaBD(arquivo,pokemonDB);
+
+        redirect('/dashboard');
+
+    }
+
+
     return(
-        <p>{id}</p>
+        <div className="create-pokemon-container">
+            <h2>{pokemonToEdit.nome}</h2>
+            <form action={updatePokemon} className="create-pokemon-form">
+                    <Image src={pokemonToEdit.img}
+                        alt=""
+                        width={100}
+                        height={100}
+                        style={{margin: "0 auto"}}
+                    /> 
+                    <section className="pokemon-input">
+                          <input type="text"
+                            id="nome"
+                            name="nome" 
+                            placeholder="Nome do Pokémon"
+                            defaultValue={pokemonToEdit.nome}
+                            />
+                    </section>
+                    <section className="pokemon-input">
+                          <input type="text"
+                            id="descricao"
+                            name="descricao" 
+                            placeholder="Descrição do Pokémon"
+                            defaultValue={pokemonToEdit.descricao}
+                            />
+                    </section>
+                    <section className="pokemon-input">
+                          <input type="text"
+                            id="img"
+                            name="img" 
+                            placeholder="Imagem do Pokémon"
+                            defaultValue={pokemonToEdit.img}
+                            />
+                    </section>
+                    <button>Atualizar</button>
+
+            </form>
+        </div>
     )
 
 }
